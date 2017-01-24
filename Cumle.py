e362bf31b0abc1149dc5293e0e36cc2422a9ce65
Abdullah <abdullahcaliskan511@gmail.com>
2017-01-24 23:22:47 +0300

    Cumle bilgileri ekleme methodu ve kelime ekleme methodu. Parent index ile kelimenin sentence index uyumlulugu

--- Cumle.py
+++ Cumle.py
@@ -1,6 +1,22 @@
-class Cumle:
+# -*- coding: utf-8 -*-
+
+
+class Sentence:
 
     def __init__(self):
-        self.cumleIndex = 0
-        self.cumleIcerik = ""
+        self.sentenceIndex = 0
+        self.sentenceContent = ""
+        self.words = []
+
+    def add_sentence_info(self, index, content):
+        self.sentenceContent = content
+        self.sentenceIndex = index
+
+    def add_word(self, word):
+        if word.parentSentenceIndex == self.sentenceIndex:
+            self.words.append(word)
+        else:
+            exit(10)
+
+
 
