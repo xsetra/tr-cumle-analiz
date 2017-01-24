cb0dea3acdf875707d7180d1fb4c083a563a608c
Abdullah <abdullahcaliskan511@gmail.com>
2017-01-25 00:21:50 +0300

    Added attribute wordFrequency. To increase this frequency value added method.

--- tranaliz/words.py
+++ tranaliz/words.py
@@ -8,9 +8,13 @@ class Word:
         self.wordIndex = 0
         self.wordContent = ""
         self.wordType = ""
+        self.wordFrequency = 0
 
     def add_word_info(self, index, content, parent_index, word_type):
         self.parentSentenceIndex = parent_index
         self.wordContent = content
         self.wordIndex = index
         self.wordType = word_type
+
+    def increase_frequency(self, value=1):
+        self.wordFrequency += value
