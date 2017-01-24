4e5978a0a4fd2f9a55c3a18578f8fa5c001f49e6
Abdullah <abdullahcaliskan511@gmail.com>
2017-01-24 23:23:17 +0300

    Kelime bilgi ekleme methodu

--- Kelime.py
+++ Kelime.py
@@ -1,3 +1,16 @@
-class Kelime:
-    parentCumleIndex = 0
+# -*- coding: utf-8 -*-
+
+
+class Word:
+    parentSentenceIndex = 0
+
     def __init__(self):
+        self.wordIndex = 0
+        self.wordContent = ""
+        self.wordType = ""
+
+    def add_word_info(self, index, content, parent_index, word_type):
+        self.parentSentenceIndex = parent_index
+        self.wordContent = content
+        self.wordIndex = index
+        self.wordType = word_type
