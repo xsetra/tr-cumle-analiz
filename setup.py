1537c80f67dd8d5512e261cbc62766342d1181b1
Ilker Manap <ilkermanap@gmail.com>
2017-01-24 22:10:09 +0100

    name and directory changes, setup.py for packaging. needs corrections on author email and license information inside setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='tranaliz',
+      version='0.1',
+      description='nlp on turkish',
+      url='https://github.com/xsetra/tr-cumle-analiz',
+      author='Abdullah Caliskan',
+      author_email='burayi duzeltirsin',
+      license='lisansi sen belirt',
+      packages=['tranaliz'],
+      zip_safe=False)
