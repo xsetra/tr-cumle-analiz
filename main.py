e6b8be7a7bc02656d0193ee5c7afc06cafea110b
Abdullah <abdullahcaliskan511@gmail.com>
2017-01-26 22:46:26 +0300

    Yapı güncellendi

--- main.py
+++ main.py
@@ -4,12 +4,12 @@
 import sys
 from tranaliz.sentences import Sentence
 from tranaliz.words import Word
+from tranaliz.analyse import Analyser
 
+tranaliz = Analyser()
 
 # Take file name on parameters
-if len(sys.argv) == 1:
-    file_name = "requirements.txt"
-else:
-    file_name = sys.argv[1]
+if len(sys.argv != 1):
+    tranaliz.fileName = sys.argv[1]
 
-file_obj = open(file_name, 'r')
+
