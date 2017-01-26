9f2519a9c8a99088dcd1e445dfe5eae4637f6ec2
Abdullah <abdullahcaliskan511@gmail.com>
2017-01-26 12:02:48 +0300

    class of word updated attributes

--- tranaliz/words.py
+++ tranaliz/words.py
@@ -8,13 +8,16 @@ class Word:
         self.wordIndex = 0
         self.wordContent = ""
         self.wordType = ""
-        self.wordFrequency = 0
+        self.wordRoot = ""
+        self.wordEclair = []
 
-    def add_word_info(self, index, content, parent_index, word_type):
+    def add_word_info(self, index, content, parent_index, word_type, root):
         self.parentSentenceIndex = parent_index
         self.wordContent = content
         self.wordIndex = index
         self.wordType = word_type
+        self.wordRoot = root
 
-    def increase_frequency(self, value=1):
-        self.wordFrequency += value
+    def add_eclair(self, eclair):
+        for addition in eclair:
+            self.wordEclair.append(addition)
