bbaa09fe7745fec7ff205403281dc79b31bbc3c1
Abdullah <abdullahcaliskan511@gmail.com>
2017-01-26 20:16:45 +0300

    Main program file

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import sys
+from tranaliz.sentences import Sentence
+from tranaliz.words import Word
 
-from Cumle import Sentence
-from Kelime import Word
 
+# Take file name on parameters
+if len(sys.argv) == 1:
+    file_name = "requirements.txt"
+else:
+    file_name = sys.argv[1]
+
+file_obj = open(file_name, 'r')
