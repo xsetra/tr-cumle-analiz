633db02412e90c5d59fdb1ed239c2e2ade385227
Abdullah <abdullahcaliskan511@gmail.com>
2017-01-26 22:46:53 +0300

    İşlevsellik için metotlara ayırma işlemi

--- tranaliz/analyse.py
+++ tranaliz/analyse.py
@@ -1,7 +1,19 @@
 # -*- coding: utf-8 -*-
 
+from sentences import Sentence
+from words import Word
 
 class Analyser:
     # Global class
-    def __init__(self):
+    def __init__(self, filename="requirements.txt"):
         self.sentences = []
+        self.content = ""
+        self.fileName = filename
+
+    def read_file(self):
+        file_obj = open(self.fileName, 'r')
+        self.content = file_obj.read()
+
+    def divide_content(self):
+        list_sentence = self.content.split('.')
+
