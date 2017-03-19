def0016761023f83d0d0d622ebef9b9c75496b60
Abdullah ÇALIŞKAN <abdullahcaliskan511@gmail.com>
2017-03-19 18:38:40 +0300

    Kelime için enumerator genişletildi. kelime içerigini veren method yazıldı

--- tranaliz/kelime.py
+++ tranaliz/kelime.py
@@ -4,10 +4,14 @@ from enum import Enum
 class KelimeTipi(Enum):
     isim = 1
     fiil = 2
+    zarf = 3
+    sifat = 4
+    ozel_isim = 5
+    baglac = 6
 
 class Kelime:
     def __init__(self):
         self.kelimeIndex = 0
         self.kelimeIcerik = ""
-        self.kelimeTipi = None
-
+        self.kelimeTipi = KelimeTipi
+        self.kelimeEk = 0
