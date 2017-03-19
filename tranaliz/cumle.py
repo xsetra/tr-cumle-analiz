a57351df29f46db2262e48e278c4d793951e0f2a
Abdullah ÇALIŞKAN <abdullahcaliskan511@gmail.com>
2017-03-19 18:38:18 +0300

    kelime ekleme , yaz ve ver methodları eklendi

--- tranaliz/cumle.py
+++ tranaliz/cumle.py
@@ -3,28 +3,33 @@
 from kelime import *
 
 class Cumle:
-    
+
     def __init__(self):
         self.cumleIndex = 0
         self.cumleIcerik = ""
-        self.cumleKelimeleri = []
+        self._cumleKelimeleri = []
 
     def kelimelere_bol(self):
         self.cumleIcerik = self.cumleIcerik.strip()
         kelime_list = self.cumleIcerik.split(' ')
         i = 1
         for k in kelime_list:
-            objKelime = Kelime()
-            objKelime.kelimeIndex = i
-            objKelime.kelimeIcerik = k
-            
-            # Burada veritabanı bağlantısı yapıp, kelimeIceriğinin tipini
-            #ögrenecegiz.
-            # TODO
-
-            self.cumleKelimeleri.append(objKelime)
+            self.kelime_ekle(k,i)
             i += 1
 
+    def kelime_ekle(self, kelime, index, tip=None):
+        objKelime = Kelime()
+        objKelime.kelimeIndex = index
+        objKelime.kelimeIcerik = kelime
+        objKelime.kelimeTipi = KelimeTipi(tip)
+        self._cumleKelimeleri.append(objKelime)
+
     def kelimeleri_yaz(self):
-        for k in self.cumleKelimeleri:
+        for k in self._cumleKelimeleri:
             print(k.kelimeIndex, k.kelimeIcerik)
+
+    def kelimeleri_ver(self):
+        satirlar = "\n"
+        for k in self._cumleKelimeleri:
+            satirlar += '\t' + str(k.kelimeIndex) + ' - ' + k.kelimeIcerik + ' - ' + str(k.kelimeTipi.name) + ' - ' + str(k.kelimeEk) + '\n'
+        return satirlar
