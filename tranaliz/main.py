752da5b0799bd2dd7a5c649630ddbb2715e53c15
Abdullah ÇALIŞKAN <abdullahcaliskan511@gmail.com>
2017-03-19 18:39:01 +0300

    Yönergeler gerçekleştirildi

--- tranaliz/main.py
+++ tranaliz/main.py
@@ -5,24 +5,12 @@
 from kelime import *
 from cumle import Cumle
 from paragraf import Paragraf
-import pymysql
-
-
-host = "localhost"
-un = "root"
-pw = "ziniba"
-db_name = "fatmabozyigit"
-
+from mysql import Veritabani
 
 dokuman = Paragraf()
-db = pymysql.connect(host, un, pw, db_name)
-cursor = db.cursor()
+db = Veritabani()
 
+db.baglan()
+db.kelime_bilgi(dokuman)
 
-dosya = open("requirements.txt", "r")
-dokuman.paragrafIcerik = dosya.read()
-
-
-dokuman.icerik_bol()
-dokuman.cumleler[0].kelimeleri_yaz()
-
+dokuman.cumleleri_yaz()
