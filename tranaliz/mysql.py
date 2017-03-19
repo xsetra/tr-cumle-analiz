9660b02e83345153963573901954452769346e4a
Abdullah ÇALIŞKAN <abdullahcaliskan511@gmail.com>
2017-03-19 18:37:45 +0300

    veritabanından kelimeler alınıp, cümle ve kelime nesnelerine aktarılıyor

--- tranaliz/mysql.py
+++ tranaliz/mysql.py
@@ -2,22 +2,30 @@
 
 
 import pymysql
+from kelime import *
+from cumle import Cumle
+from paragraf import Paragraf
 
 class Veritabani:
 	def __init__(self):
 		self.hostname = "localhost"
 		self.username = "root"
-		self.password = "----------------SECURITY-------------"
-		self.database_name = "fatmabozyigit"
+		self.password = "---SECRET---"
+		self.database_name = "fatma_turkish"
 		self.obj_db = None
 		self.cursor = None
 
 	def baglan(self):
-		self.obj_db = pymysql.connect(self.hostname,
-				     self.username,
-				     self.password,
-				     self.database_name)
-		self.cursor = obj_db.cursor()
+		self.obj_db = pymysql.connect(host=self.hostname,
+				     user=self.username,
+				     passwd=self.password,
+				     db=self.database_name,
+					 charset='latin5')
+		self.cursor = self.obj_db.cursor()
+
+	def kapat(self):
+		self.obj_db = pymysql.close()
+		print("Veritabanı bağlantısı kapatıldı.")
 
 	def kelime_tipi_sorgula(self):
 		if self.obj_db is None:
@@ -25,9 +33,24 @@ class Veritabani:
 		else:
 			# SQL sorgu cümlecikleri eklenip deger donecek
 			pass
-		
-		
-database = Veritabani()
-print(database.hostname)
+
+	def kelime_bilgi(self, paragraf):
+		query = "SELECT * FROM kelimeler"
+		self.cursor.execute(query)
+		results = self.cursor.fetchall()
+		cumle = Cumle()
+		kelime = Kelime()
+		for row in results:
+			c_index = row[4]
+			cumle = paragraf.search_index(c_index)
+			if cumle is None:
+				cumle = paragraf.cumle_ekle(index=c_index,icerik="DB Data")
+			k_index = row[5]
+			kelime = row[1]
+			k_tip = row[2]
+			cumle.kelime_ekle(index=k_index, kelime=kelime, tip=k_tip)
 
 
+if __name__ == '__main__':
+	database = Veritabani()
+	print(database.hostname)
