552e2d7c6fcf3d9a0acd5bb8c8fc132043ef6b9c
Abdullah ÇALIŞKAN <abdullahcaliskan511@gmail.com>
2017-03-19 18:39:42 +0300

    Search index ve cumle ekleme methodları eklendi

--- tranaliz/paragraf.py
+++ tranaliz/paragraf.py
@@ -1,26 +1,33 @@
 # -*- coding: utf-8 -*-
 from cumle import Cumle
 
+
 class Paragraf:
-   
+
     def __init__(self):
         self.paragrafIcerik = ""
-        self.cumleler = []
-        
+        self._cumleler = []
+
     def icerik_bol(self):
         cumle_list = self.paragrafIcerik.split('.')
-        i = 1        
+        i = 1
         for c in cumle_list:
-            objCumle = Cumle()
-            objCumle.cumleIndex = i
-            objCumle.cumleIcerik = c
-            objCumle.kelimelere_bol()
-            self.cumleler.append(objCumle)
+            self.cumle_ekle(i,c)
             i += 1
 
     def cumleleri_yaz(self):
-        for c in self.cumleler:
-            print(c.cumleIndex, c.cumleIcerik)
+        for c in self._cumleler:
+            print(c.cumleIndex, c.cumleIcerik, c.kelimeleri_ver())
 
+    def search_index(self, c_index):
+        for c in self._cumleler:
+            if c.cumleIndex == c_index:
+                return c
+        return None
 
-            
+    def cumle_ekle(self, index, icerik):
+        objCumle = Cumle()
+        objCumle.cumleIndex = index
+        objCumle.cumleIcerik = icerik
+        self._cumleler.append(objCumle)
+        return objCumle
