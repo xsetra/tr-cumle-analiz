e4790378d5623bf572cafd2b1597f1d5ce039d7e
Abdullah ÇALIŞKAN <abdullahcaliskan511@gmail.com>
2017-03-26 00:19:29 +0300

    Kelime nitelikleri için ayrintili bilgi veren metot yazıldı.

--- tranaliz/kelime.py
+++ tranaliz/kelime.py
@@ -22,6 +22,9 @@ class Kelime:
     def kelime_bilgi_ver(self):
         return str(self.kelimeIndex) + ' - ' + self.kelimeIcerik + ' - ' + str(self.kelimeTipi.name) + ' - ' + str(self.kelimeEk)
 
+    def kelime_ayrintili_bilgi_ver(self):
+        return self.kelimeIcerik + ' - ' + str(self.kelimeTipi.name) + ' - ' + str(self.kelimeEk) + ' - ' + str(self.kelimeFreq)
+
     @staticmethod
     def kelime_concat(kaynak, hedef):
         hedef.kelimeIcerik += " " + kaynak.kelimeIcerik
